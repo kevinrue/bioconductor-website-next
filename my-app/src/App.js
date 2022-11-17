6a367c6313d89f6f0b109af89c09543edd6b6008
Kevin Rue <kevinrue@users.noreply.github.com>
2022-11-17 09:12:56 +0000

    format App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <div className='App-about'>
+      <div className="App-about">
         <h1>About Bioconductor</h1>
         <p>
           The mission of the Bioconductor project is to develop, support, and
@@ -19,7 +19,8 @@ function App() {
         <p>
           Bioconductor uses the R statistical programming language, and is open
           source and open development. It has two releases each year, and an
-          active user community. Bioconductor is also available as Docker images.
+          active user community. Bioconductor is also available as Docker
+          images.
         </p>
       </div>
     </div>
