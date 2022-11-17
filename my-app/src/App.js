c2dc65ae110f27f146fedf8eddb01376dbf977e6
Kevin Rue <kevinrue@users.noreply.github.com>
2022-11-17 12:13:32 +0000

    refactor app header into separate component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
-import logo from "./bioconductor_logo_rgb.svg";
 import "./App.css";
+import AppHeader from "./AppHeader";
 import AppAbout from "./AppAbout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <AppHeader />
       <AppAbout />
     </div>
   );
