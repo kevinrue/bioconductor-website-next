a8afa1ccdfeb25ca9c5abe814b824277002823d2
Kevin Rue <kevinrue@users.noreply.github.com>
2022-11-17 08:19:17 +0000

    Default .eslintrc.js files to node environment

---
+++ my-app/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md#when-not-to-use-it
+        "plugin:react/jsx-runtime",
+    ],
+    // https://github.com/eslint/eslint/issues/13008
+    "overrides": [{
+        "files": [
+            ".eslintrc.js",
+        ],
+        "env": {
+            "node": true,
+            "browser": false,
+        },
+    }],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+          },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-extra-semi":"error"
+    }
+}
