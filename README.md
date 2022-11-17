bf4e89b6929dabdde0910ca6cc6587fd89c0168f
Kevin Rue <kevinrue@users.noreply.github.com>
2022-11-17 09:12:25 +0000

    add about section on landing page

--- README.md
+++ README.md
@@ -2,8 +2,38 @@
 
 In development.
 
-## Set up
+## Initial set up
 
 Following tutorial at <https://code.visualstudio.com/docs/nodejs/reactjs-tutorial>
 
-- `npx create-react-app my-app`
+- Install Node.js from <https://nodejs.org/en/>.
+- Open Visual Studio Code.
+- (Terminal) `npx create-react-app my-app`: create template files.
+- (Terminal) `cd my-app`: .
+- (Terminal) `npm start`.
+- Visual Studio Extensions:
+  - 
+
+## Set up for contributors
+
+- Clone repository from GitHub.
+- Open workspace in Visual Studio Code.
+
+## Preview & Testing
+
+- `Ctrl` + `Shift` + `V`: Preview Markdown documents (applies to the document currently open).
+
+- Terminal: `npm start` (start the web server).
+- `F5`: Run and Debug (using the configuration in `.vscode.json`).
+
+## Formatting
+
+- `Alt` + `Shift` + `F`: Format Document (applies to the document currently open).
+
+## Debugging
+
+Initial configuration:
+
+- `Ctrl` + `Shift` + `D`: Open the 'Run and Debug' panel.
+- Click on 'select the create a launch.json file link'.
+- Change the port of the url from `8080` to `3000`.
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-import logo from './bioconductor_logo_rgb.svg';
-import './App.css';
+import logo from "./bioconductor_logo_rgb.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,6 +7,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div className='App-about'>
+        <h1>About Bioconductor</h1>
+        <p>
+          The mission of the Bioconductor project is to develop, support, and
+          disseminate free open source software that facilitates rigorous and
+          reproducible analysis of data from current and emerging biological
+          assays. We are dedicated to building a diverse, collaborative, and
+          welcoming community of developers and data scientists.
+        </p>
+        <p>
+          Bioconductor uses the R statistical programming language, and is open
+          source and open development. It has two releases each year, and an
+          active user community. Bioconductor is also available as Docker images.
+        </p>
+      </div>
     </div>
   );
 }
