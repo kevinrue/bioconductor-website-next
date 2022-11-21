51d7f3d7444a8c7c4c50eed425078566ddb2ba15
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-11-21 11:34:06 +0000

    center and adapt width of post to screen size

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,8 +5,9 @@ import styles from "../../styles/Post.module.css";
 import Layout from "../../components/layout";
 import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Grid from '@mui/material/Grid';
 import ReactMarkdown from "react-markdown";
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 export default function Post({ postData }) {
   return (
@@ -20,31 +21,35 @@ export default function Post({ postData }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>{postData.title}</h1>
-        <p className={styles.author}>By {postData.author}</p>
-        <ReactMarkdown
-          className={styles.content}
-          skipHtml={true}
-          components={{
-            code: ({ node, inline, className, children, ...props }) => {
-              const match = /language-(\w+)/.exec(className || "");
-              return !inline && match ? (
-                <SyntaxHighlighter language={match[1]} {...props}>
-                  {String(children)}
-                </SyntaxHighlighter>
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              );
-            },
-          }}
-        >
-          {postData.content}
-        </ReactMarkdown>
-        <p className={styles.date}>
-          Created on {postData.created} (Last updated: {postData.edited})
-        </p>
+        <Grid container className={styles.post}>
+          <Grid item xs={12} md={9}>
+            <h1>{postData.title}</h1>
+            <p className={styles.author}>By {postData.author}</p>
+            <ReactMarkdown
+              className={styles.content}
+              skipHtml={true}
+              components={{
+                code: ({ node, inline, className, children, ...props }) => {
+                  const match = /language-(\w+)/.exec(className || "");
+                  return !inline && match ? (
+                    <SyntaxHighlighter language={match[1]} {...props}>
+                      {String(children)}
+                    </SyntaxHighlighter>
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+            >
+              {postData.content}
+            </ReactMarkdown>
+            <p className={styles.date}>
+              Created on {postData.created} (Last updated: {postData.edited})
+            </p>
+          </Grid>
+        </Grid>
       </main>
     </Layout>
   );
