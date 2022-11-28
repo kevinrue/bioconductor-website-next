d669d88747c0fe0f05259954bf29ee8b1d5e8115
Kevin Rue <kevinrue@users.noreply.github.com>
2022-11-28 22:05:13 +0000

    fix fix in previous commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@
 import "bootstrap/dist/css/bootstrap.css";
 // Import custom global stylesheet to override aspects of bootstrap
 import "../styles/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 import type { AppProps } from "next/app";
 import { SSRProvider } from "react-aria";
 
