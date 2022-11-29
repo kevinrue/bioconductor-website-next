6d3453310dd4c61bf3c99e34c81d412b0390e8b4
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-11-29 16:45:58 +0000

    rewrite get started page in rmarkdown

--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -54,65 +54,39 @@ export default function Releases() {
       <main className={styles.main}>
         <Grid container className={styles.grid}>
           <Grid item xs={grid_item_xs} md={grid_item_md}>
-            <h1>Get started</h1>
-            <h2>Welcome</h2>
-            <p>Welcome to the Bioconductor community!</p>
+
             <p>
-              This guide details how to install the latest release of
-              Bioconductor on your computer.
-            </p>
-            <h2>Pre-requisites</h2>
-            <h3>Install {fa_r_project}</h3>
-            <p>
-              The current release of Bioconductor is version{" "}
-              {latestBiocReleaseData.version}; it works with {fa_r_project}{" "}
-              version {latestBiocReleaseData.r_version}. Users of older{" "}
-              {fa_r_project} versions must update their installation to take
-              advantage of new features and to access packages that have been
-              added to Bioconductor since the last release.
+
             </p>
             <p>
-              Follow the instructions for your operating system in{" "}
-              <Link href="https://carpentries-incubator.github.io/bioc-intro/setup.html">
-                this episode
-              </Link>{" "}
-              of the Carpentries lesson &quot;Introduction to data analysis with
-              R and Bioconductor&quot;. The page also contains information to
-              check which version of {fa_r_project} you are using; if you
-              already have the correct version, you may not need to do anything!
+
             </p>
-            <h2>Install the BiocManager package</h2>
+            <h2></h2>
             <p>
-              The <code>BiocManager</code> package is a convenient tool
-              distributed on the{" "}
-              <Link href="https://cran.r-project.org/">CRAN</Link> repository to
-              install and update Bioconductor packages.
+
             </p>
             <p>
-              To install the package, type the following in an {fa_r_project}{" "}
-              console:
+
             </p>
             <SyntaxHighlighter className={styles.codeblock} language="r">
-              {'install.packages("BiocManager")'}
+              {''}
             </SyntaxHighlighter>
-            <h2>Install the current Bioconductor release</h2>
+            <h2></h2>
             <p>
-              To install the latest version of Bioconductor core packages, type
-              the following in an R console:
+
             </p>
             <SyntaxHighlighter className={styles.codeblock} language="r">
-              {`BiocManager::install(version = "${latestBiocReleaseData.version}")`}
+              {``}
             </SyntaxHighlighter>
-            <h2>Check your installation</h2>
+            <h2></h2>
             <p>
-              To check your installation, restart your {fa_r_project} session,
-              and type the following in an R console:
+
             </p>
             <SyntaxHighlighter className={styles.codeblock} language="r">
-              {"BiocManager::version()"}
+              {""}
             </SyntaxHighlighter>
             <p>
-              The answer should be &lsquo;{latestBiocReleaseData.version}&rsquo;
+
             </p>
             <p>
               Congratulations! You are ready to install the latest version of
