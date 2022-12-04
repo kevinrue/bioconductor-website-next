f95a7fd6f1970ac0c4ee286578e5b95d0b4c96e9
Kevin Rue <kevinrue@users.noreply.github.com>
2022-12-04 14:33:09 +0000

    remove remove br tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <meta
           name="description"
           content="The mission of the Bioconductor project is to develop, support, and disseminate free open source software that facilitates rigorous and reproducible analysis of data from current and emerging biological assays. We are dedicated to building a diverse, collaborative, and welcoming community of developers and data scientists.
-          <br />
+          
           Bioconductor uses the R statistical programming language, and is open source and open development. It has two releases each year, and an active user community. Bioconductor is also available as Docker images."
         />
         <link rel="icon" href="/favicon.ico" />
